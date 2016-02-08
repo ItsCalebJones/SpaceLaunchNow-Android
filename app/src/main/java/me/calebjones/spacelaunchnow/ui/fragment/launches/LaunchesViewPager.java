8f71517ea9811e158988ac03e20ceb3465d2d1cb
Caleb Jones <cjones@tripwire.com>
2016-02-08 16:10:05 -0500

    Fix duplicate Favorites
    Squashed another bug with syncing list of favorites.
    Testing change to FragmentPagerAdapter.

--- app/src/main/java/me/calebjones/spacelaunchnow/ui/fragment/launches/LaunchesViewPager.java
+++ app/src/main/java/me/calebjones/spacelaunchnow/ui/fragment/launches/LaunchesViewPager.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -121,7 +121,7 @@ public class LaunchesViewPager extends Fragment {
     }
 
 
-    public class PagerAdapter extends FragmentStatePagerAdapter {
+    public class PagerAdapter extends FragmentPagerAdapter {
         int mNumOfTabs;
 
         public PagerAdapter(FragmentManager fm, int NumOfTabs) {
