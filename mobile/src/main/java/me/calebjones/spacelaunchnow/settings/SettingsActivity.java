a76e88224e2abbfd23028e42140fed599754fef0
Caleb Jones <cjones@alpcjonesm.tripwire.com>
2016-09-12 14:12:51 -0400

    Remove v7-preference dependency.

--- mobile/src/main/java/me/calebjones/spacelaunchnow/settings/SettingsActivity.java
+++ mobile/src/main/java/me/calebjones/spacelaunchnow/settings/SettingsActivity.java
@@ -1,10 +1,34 @@
 package me.calebjones.spacelaunchnow.settings;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
+
+import de.mrapp.android.preference.activity.PreferenceActivity;
 import me.calebjones.spacelaunchnow.BuildConfig;
 import me.calebjones.spacelaunchnow.R;
 
 
-public class SettingsActivity extends BasePreferenceActivity {
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public final void onStart() {
+        super.onStart();
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
 
     @Override
     protected final void onCreatePreferenceHeaders() {
