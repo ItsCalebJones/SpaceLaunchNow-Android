d158c2d5864e202838f35583f3a1e8778ec51bee
Caleb Jones <ca.jones9119@gmail.com>
2018-02-15 14:10:15 -0500

    Fix crash on 4.0

--- mobile/src/main/java/me/calebjones/spacelaunchnow/content/receivers/BootReceiver.java
+++ mobile/src/main/java/me/calebjones/spacelaunchnow/content/receivers/BootReceiver.java
@@ -4,12 +4,12 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.time.zone.ZoneOffsetTransitionRule;
+
 import me.calebjones.spacelaunchnow.content.data.DataRepositoryManager;
 
 public class BootReceiver extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
-        DataRepositoryManager dataRepositoryManager = new DataRepositoryManager(context);
-        dataRepositoryManager.syncBackground();
     }
 }
