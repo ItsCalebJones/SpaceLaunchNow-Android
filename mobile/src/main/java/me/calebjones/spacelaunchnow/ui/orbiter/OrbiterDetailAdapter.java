046ca27c5cf49ec4ca2e16b1077bc30e3cc79e81
Caleb Jones <ca.jones9119@gmail.com>
2018-09-11 23:05:43 -0400

    Work in Progress - Orbiters.

--- mobile/src/main/java/me/calebjones/spacelaunchnow/ui/orbiter/OrbiterDetailAdapter.java
+++ mobile/src/main/java/me/calebjones/spacelaunchnow/ui/orbiter/OrbiterDetailAdapter.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.load.DataSource;
@@ -19,9 +18,11 @@ import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+import at.blogc.android.views.ExpandableTextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -39,6 +40,7 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
     private List<Orbiter> items;
     private RequestOptions requestOptions;
     private int backgroundColor = 0;
+    private SimpleDateFormat sdf;
 
     public OrbiterDetailAdapter(Context context, Activity activity) {
         items = new ArrayList<>();
@@ -47,6 +49,8 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
                 .centerCrop();
         this.context = context;
         this.activity = activity;
+        sdf = Utils.getSimpleDateFormatForUI("MMMM yyyy");
+        sdf.toLocalizedPattern();
     }
 
     public void addItems(List<Orbiter> items) {
@@ -75,9 +79,8 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
     public void onBindViewHolder(final ViewHolder holder, int i) {
         Orbiter orbiter = items.get(holder.getAdapterPosition());
 
-
         //Set up vehicle card Information
-        holder.orbiterTitle.setText(String.format("%s Spacecraft", orbiter.getName()));
+        holder.orbiterTitle.setText(String.format(context.getString(R.string.spacecraft_format), orbiter.getName()));
         holder.orbiterSubtitle.setText(orbiter.getAgency());
 
         holder.orbiterName.setText(String.format(context.getString(R.string.spacecraft_details), orbiter.getName()));
@@ -90,6 +93,58 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
             holder.orbiterTitle.setBackgroundColor(backgroundColor);
             holder.orbiterSubtitle.setBackgroundColor(backgroundColor);
         }
+
+        if (orbiter.getDiameter() != null) {
+            holder.diameter.setText(String.format(context.getString(R.string.diameter_full), orbiter.getDiameter()));
+        }
+        if (orbiter.getHeight() != null) {
+            holder.height.setText(String.format(context.getString(R.string.height_full), orbiter.getHeight()));
+        }
+        if (orbiter.getPayloadCapacity() != null) {
+            holder.payload.setText(String.format(context.getString(R.string.payload), orbiter.getPayloadCapacity()));
+        }
+
+        if (orbiter.getFlightLife() != null) {
+            holder.flightLife.setVisibility(View.VISIBLE);
+            holder.flightLife.setText(orbiter.getFlightLife());
+        } else {
+            holder.flightLife.setVisibility(View.GONE);
+        }
+
+        if (orbiter.getInUse()){
+            GlideApp.with(context)
+                    .load(R.drawable.ic_checkmark)
+                    .into(holder.activeIcon);
+        } else {
+            GlideApp.with(context)
+                    .load(R.drawable.ic_failed)
+                    .into(holder.activeIcon);
+        }
+
+        if (orbiter.getHumanRated() == null ){
+            GlideApp.with(context)
+                    .load(R.drawable.ic_question_mark)
+                    .into(holder.crewIcon);
+            holder.crewCapacity.setVisibility(View.GONE);
+        } else if (orbiter.getHumanRated()){
+            GlideApp.with(context)
+                    .load(R.drawable.ic_checkmark)
+                    .into(holder.crewIcon);
+            holder.crewCapacity.setVisibility(View.VISIBLE);
+            holder.crewCapacity.setText(String.format(context.getString(R.string.crew_capacity), orbiter.getCrewCapacity()));
+        } else {
+            holder.crewCapacity.setVisibility(View.GONE);
+            GlideApp.with(context)
+                    .load(R.drawable.ic_failed)
+                    .into(holder.crewIcon);
+        }
+
+        if (orbiter.getMaidenFlight() != null) {
+            holder.firstFlight.setText(sdf.format(orbiter.getMaidenFlight()));
+        } else {
+            holder.firstFlight.setText(R.string.unknown);
+        }
+
         GlideApp.with(context)
                 .load(orbiter.getImageURL())
                 .placeholder(R.drawable.placeholder)
@@ -111,9 +166,17 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
 
 
         if (orbiter.getWikiLink() != null && orbiter.getWikiLink().length() > 0) {
+            holder.wikiButton.setVisibility(View.VISIBLE);
             holder.wikiButton.setOnClickListener(v -> Utils.openCustomTab(activity, context, orbiter.getWikiLink()));
         } else {
-            holder.wikiButton.setVisibility(View.INVISIBLE);
+            holder.wikiButton.setVisibility(View.GONE);
+        }
+
+        if (orbiter.getInfoLink() != null && orbiter.getInfoLink().length() > 0) {
+            holder.infoButton.setVisibility(View.VISIBLE);
+            holder.infoButton.setOnClickListener(v -> Utils.openCustomTab(activity, context, orbiter.getInfoLink()));
+        } else {
+            holder.infoButton.setVisibility(View.GONE);
         }
     }
 
@@ -132,27 +195,46 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
     }
 
 
-
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         @BindView(R.id.orbiter_image)
         ImageView orbiterImage;
+        @BindView(R.id.in_use_icon)
+        ImageView activeIcon;
+        @BindView(R.id.human_rated_icon)
+        ImageView crewIcon;
         @BindView(R.id.orbiter_title)
         TextView orbiterTitle;
         @BindView(R.id.orbiter_subtitle)
         TextView orbiterSubtitle;
         @BindView(R.id.orbiter_name)
         TextView orbiterName;
+        @BindView(R.id.orbiter_description_expand)
+        View orbiterDescriptionExpand;
         @BindView(R.id.orbiter_description)
-        TextView orbiterDescription;
-        @BindView(R.id.orbiter_description_layout)
-        LinearLayout orbiterDescriptionLayout;
+        ExpandableTextView orbiterDescription;
         @BindView(R.id.orbiter_history)
         TextView orbiterHistory;
         @BindView(R.id.orbiter_history_description)
-        TextView orbiterHistoryDescription;
+        ExpandableTextView orbiterHistoryDescription;
+        @BindView(R.id.orbiter_history_expand)
+        View orbiterHistoryExpand;
         @BindView(R.id.wikiButton)
         AppCompatButton wikiButton;
+        @BindView(R.id.infoButton)
+        AppCompatButton infoButton;
+        @BindView(R.id.diameter)
+        TextView diameter;
+        @BindView(R.id.height)
+        TextView height;
+        @BindView(R.id.payload)
+        TextView payload;
+        @BindView(R.id.crew_capacity)
+        TextView crewCapacity;
+        @BindView(R.id.flight_life)
+        TextView flightLife;
+        @BindView(R.id.first_flight_text)
+        TextView firstFlight;
 
         //Add content to the card
         public ViewHolder(View view) {
@@ -160,8 +242,24 @@ public class OrbiterDetailAdapter extends RecyclerView.Adapter<OrbiterDetailAdap
             ButterKnife.bind(this, view);
         }
 
-        @OnClick(R.id.wikiButton)
-        public void onViewClicked() {
+        @OnClick(R.id.orbiter_history_expand)
+        public void onHistoryViewClicked() {
+            orbiterHistoryDescription.toggle();
+            if (orbiterHistoryDescription.isExpanded()) {
+                orbiterHistoryExpand.animate().rotation(0).start();
+            } else {
+                orbiterHistoryExpand.animate().rotation(180).start();
+            }
+        }
+
+        @OnClick(R.id.orbiter_description_expand)
+        public void onDescriptionViewClicked() {
+            orbiterDescription.toggle();
+            if (orbiterDescription.isExpanded()) {
+                orbiterDescriptionExpand.animate().rotation(0).start();
+            } else {
+                orbiterDescriptionExpand.animate().rotation(180).start();
+            }
         }
     }
 }
