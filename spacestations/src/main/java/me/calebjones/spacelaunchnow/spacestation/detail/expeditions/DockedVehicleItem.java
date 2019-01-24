f0413e5da9db3ef017c72a13d49034c5332f7704
Caleb Jones <ca.jones9119@gmail.com>
2019-01-24 09:18:27 -0500

    Start work on details.

--- spacestations/src/main/java/me/calebjones/spacelaunchnow/spacestation/detail/expeditions/DockedVehicleItem.java
+++ spacestations/src/main/java/me/calebjones/spacelaunchnow/spacestation/detail/expeditions/DockedVehicleItem.java
@@ -5,9 +5,6 @@ import me.calebjones.spacelaunchnow.data.models.main.spacecraft.SpacecraftStage;
 public class DockedVehicleItem extends SpacecraftStage implements ListItem {
 
     @Override
-    public
-
-    @Override
     public int getListItemType() {
         return ListItem.TYPE_DOCKED_VEHICLE;
     }
